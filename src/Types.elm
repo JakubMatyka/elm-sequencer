8de6f6d7f72d72077419dc6593bedb6fd669810d
jaqbec <jaqbec@gmail.com>
2018-05-03 02:22:21 +0200

    feat(*): delete unnecessary code and start small

--- src/Types.elm
+++ src/Types.elm
@@ -2,29 +2,17 @@ import Array exposing (..)
 import Set exposing (..)
 
 type alias Model =
-    { tracks: Array Track
-    , playback: Playback
-    , playbackPosition: PlaybackPosition
-    , bpm: Int
-    , playbackSequence: Array (Set Clip)
+    { track : Track
     }
 
 type alias Track =
-    { name: String
-    , clip: Clip
-    , sequence: Array Step
+    { name : String
+    , sequence : Array Step
     }
 
 type Step
-    = Active
-    | Inactive
+    = On
+    | Off
 
-type Playback
-    = Playing
-    | Stopped
-
-type alias Clip
-    = String
-
-type alias PlaybackPosition
-    = Int
+type alias Clip =
+    String
